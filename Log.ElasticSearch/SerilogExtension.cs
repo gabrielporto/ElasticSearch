5545e10e287b0c545083fa967d1dd2565a28cc90
Gabriel Porto Lopes <gabriel.lopes@lojasrede.com.br>
2023-07-10 17:44:23 -0300

    wrok

---
+++ Log.ElasticSearch/SerilogExtension.cs
@@ -0,0 +1,54 @@
+﻿using Serilog.Events;
+using Serilog.Filters;
+using Serilog;
+using Microsoft.Extensions.Configuration;
+
+namespace Api.A
+{
+    public class SerilogExtension
+    {
+        public WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder, IConfiguration configuration, string applicationName)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("System", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("ApplicationName", $"{applicationName} - {Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")}")
+                .Enrich.WithCorrelationId()
+                .Enrich.WithExceptionDetails()
+                .Filter.ByExcluding(Matching.FromSource("Microsoft.AspNetCore.StaticFiles"))
+                .WriteTo.Async(writeTo => writeTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticsearchSettings:uri"]))
+                {
+                    TypeName = null,
+                    AutoRegisterTemplate = true,
+                    IndexFormat = configuration["ElasticsearchSettings:defaultIndex"],
+                    BatchAction = ElasticOpType.Create,
+                    CustomFormatter = new EcsTextFormatter(),
+                    ModifyConnectionSettings = x => x.BasicAuthentication(configuration["ElasticsearchSettings:username"], configuration["ElasticsearchSettings:password"])
+                }))
+                .WriteTo.Async(writeTo => writeTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}"))
+                .CreateLogger();
+
+            builder.Logging.ClearProviders();
+            builder.Host.UseSerilog(Log.Logger, true);
+
+            return builder;
+        }
+
+        public static WebApplication UseSerilog(this WebApplication app)
+        {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+            app.UseMiddleware<RequestSerilLogMiddleware>();
+
+            app.UseSerilogRequestLogging(opts =>
+            {
+                opts.EnrichDiagnosticContext = LogEnricherExtensions.EnrichFromRequest;
+            });
+
+            return app;
+        }
+
+
+    }
+}
